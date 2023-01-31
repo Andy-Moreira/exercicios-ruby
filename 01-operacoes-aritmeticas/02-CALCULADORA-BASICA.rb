2500947511eb964e6413458970c301a1413a412b
Andreia Cristine Moreira <andreiacristinemoreira@gmail.com>
2023-01-31 21:19:00 +0000

    Alteraçao arredondamento para cima

--- 01-operacoes-aritmeticas/02-CALCULADORA-BASICA.rb
+++ 01-operacoes-aritmeticas/02-CALCULADORA-BASICA.rb
@@ -6,6 +6,6 @@ num2 = gets.chomp.to_i
 
 puts "\nSoma      = #{num1 + num2} "
 puts "Subtração   = #{num1 - num2} "
-puts "Divisão     = #{num1.to_f / num2.to_f} "
+puts "Divisão     = #{(num1.to_f / num2.to_f).ceil(2)} "
 puts "Resto       = #{num1 % num2} "
 puts "Potenciação = #{num1 ** num2} "
