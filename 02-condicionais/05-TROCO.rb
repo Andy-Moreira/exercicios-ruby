2e905e976376958e929a9e6900f49135b37918fd
Andreia Cristine Moreira <andreiacristinemoreira@gmail.com>
2023-02-01 15:21:34 +0000

    Alteração exercício troco

---
+++ 02-condicionais/05-TROCO.rb
@@ -0,0 +1,19 @@
+print "Preço unitário do produto: "
+preco = gets.chomp.to_f
+
+print "Quantidade comprada: "
+qtd_comprada = gets.chomp.to_i
+
+print "Dinheiro recebido: "
+dinheiro_recebido = gets.chomp.to_f
+
+total = qtd_comprada * preco
+
+if (dinheiro_recebido >= total)
+  troco = dinheiro_recebido - total
+  print "\nTroco = R$  ", troco
+
+else
+  troco = total - dinheiro_recebido
+  print "\nDinheiro insuficiente! Faltam: R$ ", troco
+end
