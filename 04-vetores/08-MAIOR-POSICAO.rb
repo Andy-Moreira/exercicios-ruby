a7d6bd6a499d2b879ca1704c8a22678ead91c14e
Andreia Moreira <andreiacristinemoreira@gmail.com>
2023-02-28 21:39:58 +0000

    Exercício maior posiçAo

---
+++ 04-vetores/08-MAIOR-POSICAO.rb
@@ -0,0 +1,14 @@
+print "São quantos números? "
+n = gets.chomp.to_i
+puts
+
+numbers = []
+n.times do |i|
+  print "Digite o #{i + 1}º número: "
+  numbers[i] = gets.chomp.to_f
+end
+
+puts
+puts "MAIOR VALOR = #{numbers.max}"
+puts "POSIÇÃO DO MAIOR VALOR = #{numbers.index(numbers.max)}"
+puts
